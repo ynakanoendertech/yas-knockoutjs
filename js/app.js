83c76e0f8e19b9822caa2d7f6b19fa3f9bc0ebe0
Yasuhiro Nakano <ynakano@endertech.com>
2016-11-25 15:43:18 -0800

    Load jquery via requirejs

---
+++ js/app.js
@@ -0,0 +1,8 @@
+"use strict";
+
+require(['jquery'], function($) {
+
+    $(document).ready(function() {
+        console.log('app.js');
+    });
+});
---
+++ js/config.js
@@ -0,0 +1,8 @@
+"use strict";
+
+requirejs.config({
+    baseUrl: '.',
+    paths: {
+        jquery: 'node_modules/jquery/dist/jquery'
+    }
+});
