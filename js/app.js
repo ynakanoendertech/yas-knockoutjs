bb81475920424a28fd2429565939b653f44bd3eb
Yasuhiro Nakano <ynakano@endertech.com>
2016-11-25 15:59:48 -0800

    Add view model in app.js

--- js/app.js
+++ js/app.js
@@ -3,6 +3,15 @@
 require(['jquery', 'ko',], function($, ko) {
 
     $(document).ready(function() {
+
+        // View model
+        function AppViewModel() {
+            this.firstName = "Bert";
+            this.lastName = "Bertington";
+        }
+
+        // Activate
+        ko.applyBindings(new AppViewModel());
     });
 
 });
