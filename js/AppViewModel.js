165a0361958113249bcecfb8beccd2c484837722
Yasuhiro Nakano <ynakano@endertech.com>
2016-11-28 15:15:29 -0800

    Clean up index.html and AppViewModel.js

--- js/AppViewModel.js
+++ js/AppViewModel.js
@@ -3,24 +3,6 @@ define(['ko'], function(ko) {
 
     // View model
     return function AppViewModel() {
-        
-        // Preserve this
-        var self = this;
-        
-        self.firstName = ko.observable("Bert");
-        self.lastName = ko.observable("Bertington");
-        self.test = 1;
 
-        console.dir(typeof self.test);
-        console.dir(typeof self.firstName);
-
-        self.fullName = ko.computed(function() {
-            return self.firstName() + " " + self.lastName();
-        });  // Defines the value of this, which is function c() of KnockoutJs
-
-        self.capitalizeLastName = function() {
-            var currentVal = self.lastName();
-            self.lastName(currentVal.toUpperCase());
-        };
     };
 });
