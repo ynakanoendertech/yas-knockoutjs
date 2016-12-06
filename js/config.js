2945e67e3e27bf1103425d30c714ba5afecb57d9
Yasuhiro Nakano <ynakano@endertech.com>
2016-12-06 13:56:51 -0800

    Use debug version of ko to use ko.dependencyDetection

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@ requirejs.config({
     baseUrl: '.',
     paths: {
         jquery: 'node_modules/jquery/dist/jquery',
-        ko: 'node_modules/knockout/build/output/knockout-latest',
+        ko: 'node_modules/knockout/build/output/knockout-latest.debug',
         AppViewModel: "js/AppViewModel"
     }
 });
