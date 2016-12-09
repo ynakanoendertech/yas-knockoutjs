960fd20ffdd9f46486d1abc72ea32945fc7df6cb
Yasuhiro Nakano <ynakano@endertech.com>
2016-12-08 17:52:47 -0800

    Decendant elements are dynamically added or removed

--- js/AppViewModel.js
+++ js/AppViewModel.js
@@ -8,11 +8,23 @@ define(['ko'], function(ko) {
         var self = this;
 
         // Instance properties
+        self.twitterName = ko.observable('@test');
+        self.resultData = ko.observable();
 
-        self.city = "London";
-        self.coords = {
-            latitude:  51.5001524,
-            longitude: -0.1262362
+        // Instance methods
+        self.getTweets = function() {
+            var name = self.twitterName(),
+                simulatedResults = [
+                    { text: name + ' What a nice day.' },
+                    { text: name + ' Building some cool apps.' },
+                    { text: name + ' Just saw a famous celebrity eating.' }
+                ];
+
+            self.resultData({ retrievalDate: new Date(), topTweets: simulatedResults });
         };
+        self.clearResults = function() {
+            self.resultData(undefined);
+        };
+
     };
 });
