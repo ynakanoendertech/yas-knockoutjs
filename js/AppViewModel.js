6057bf697bd2c55765514dc5d562e93580acfde4
Yasuhiro Nakano <ynakano@endertech.com>
2016-12-08 17:29:04 -0800

    With switches the binding context to a child object

--- js/AppViewModel.js
+++ js/AppViewModel.js
@@ -4,5 +4,15 @@ define(['ko'], function(ko) {
     // View model
     return function AppViewModel() {
 
+        // Preserve this
+        var self = this;
+
+        // Instance properties
+
+        self.city = "London";
+        self.coords = {
+            latitude:  51.5001524,
+            longitude: -0.1262362
+        };
     };
 });
