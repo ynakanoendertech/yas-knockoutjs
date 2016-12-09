77c71a287145c8a2c94f0dd0109a5a96a969ab16
Yasuhiro Nakano <ynakano@endertech.com>
2016-12-09 14:36:15 -0800

    Use a simple object literal as a view model

--- js/AppViewModel.js
+++ js/AppViewModel.js
@@ -2,21 +2,9 @@ define(['ko'], function(ko) {
     "use strict";
 
     // View model
-    return function AppViewModel(items) {
-
-        // Preserve this
-        var self = this;
-
-        // Instalnce properties
-        self.itemToAdd = ko.observable("");  // init with ""
-        self.myItems = ko.observableArray(items);
-
-        // Instance methods
-        self.addItem = function() {
-            if (self.itemToAdd() != "") {
-                self.myItems.push(self.itemToAdd());
-                self.itemToAdd("");  // reset with ""
-            }
-        };
+    var AppViewModel = {
+        personName: 'Bob',
+        personAge: 123
     };
+    return AppViewModel;
 });
--- js/app.js
+++ js/app.js
@@ -5,6 +5,6 @@ require(['jquery', 'ko', 'AppViewModel'], function($, ko, AppViewModel) {
     $(document).ready(function() {
 
         // Activate
-        ko.applyBindings(new AppViewModel(['apple', 'banana', 'cherry']));
+        ko.applyBindings(AppViewModel);
     });
 });
