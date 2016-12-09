54db43a40e876fea1c30a553bf8ceab878210e0c
Yasuhiro Nakano <ynakano@endertech.com>
2016-12-09 14:48:08 -0800

    Explicitly subscribing to observables

--- js/AppViewModel.js
+++ js/AppViewModel.js
@@ -7,9 +7,15 @@ define(['ko'], function(ko) {
         personAge: ko.observable(20),
     };
 
+    // Subscribe to value change on personName
+    var subscription = AppViewModel.personName.subscribe(function(newValue) {
+        console.log("The persons's new name is " + newValue);
+    });
+
     setTimeout(function() {
         console.log('updated');
         AppViewModel.personName('Updated').personAge(30);
+        subscription.dispose();  // Terminate a subscription
     }, 3000);
     return AppViewModel;
 });
