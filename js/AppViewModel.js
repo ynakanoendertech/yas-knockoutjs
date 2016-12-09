f80d6c35e1c5bc658c0df81aed85b556c22917c9
Yasuhiro Nakano <ynakano@endertech.com>
2016-12-09 14:42:54 -0800

    Use chaining syntax

--- js/AppViewModel.js
+++ js/AppViewModel.js
@@ -4,12 +4,12 @@ define(['ko'], function(ko) {
     // View model
     var AppViewModel = {
         personName: ko.observable('Bob'),
-        personAge: ko.observable(123)
+        personAge: ko.observable(20),
     };
 
     setTimeout(function() {
         console.log('updated');
-        AppViewModel.personName('Updated');
+        AppViewModel.personName('Updated').personAge(30);
     }, 3000);
     return AppViewModel;
 });
