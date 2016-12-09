fa9bf67e98c4a2d578ab6292fb3e33d6928c09fe
Yasuhiro Nakano <ynakano@endertech.com>
2016-12-09 15:37:23 -0800

    Define read and write functions for a computed observable

--- js/AppViewModel.js
+++ js/AppViewModel.js
@@ -12,8 +12,18 @@ define(['ko'], function(ko) {
         self.lastName = ko.observable("Smith");
 
         // Instance methods
-        self.fullName = ko.computed(function() {
-            return self.firstName() + " " + self.lastName();
+        self.fullName = ko.pureComputed({
+            read: function() {
+                return self.firstName() + " " + self.lastName();
+            },
+            write: function(value) {
+                var lastSpacePos = value.lastIndexOf(" ");
+                if (lastSpacePos > 0) {
+                    this.firstName(value.substring(0, lastSpacePos));
+                    this.lastName(value.substring(lastSpacePos + 1));
+                }
+            },
+            owner: self
         });
     }
 
