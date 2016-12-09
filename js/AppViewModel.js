dee250074a58f588001cfb0fb39b24b5b2961d57
Yasuhiro Nakano <ynakano@endertech.com>
2016-12-08 16:54:42 -0800

    Observable variables are functions and need to use parenthesis

--- js/AppViewModel.js
+++ js/AppViewModel.js
@@ -4,5 +4,23 @@ define(['ko'], function(ko) {
     // View model
     return function AppViewModel() {
 
+        // Preserve this
+        var self = this;
+
+        // Instance properties
+
+        self.firstName = ko.observable("Bob");
+        self.lastName = ko.observable("Smith");
+
+        // Instance methods
+
+        self.fullNameWrong = ko.computed(function() {
+            return self.firstName + " " + self.lastName;
+        });
+
+        self.fullName = ko.computed(function() {
+            return self.firstName() + " " + self.lastName();
+        });
+
     };
 });
