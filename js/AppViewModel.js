ed988aaea7e1f672c77809099c03cc736811a750
Yasuhiro Nakano <ynakano@endertech.com>
2016-12-13 11:49:03 -0800

    Clean up

--- js/AppViewModel.js
+++ js/AppViewModel.js
@@ -4,19 +4,6 @@ define(['ko'], function(ko) {
     // View model
     function AppViewModel() {
 
-        // Preserve this
-        var self = this;
-
-        self.currentProfit = ko.observable(150000);
-
-        self.isSevere = ko.pureComputed(function() {
-            return self.currentProfit() < 0;
-        });
-
-        setTimeout(function() {
-            console.log('Change ' + self.currentProfit() + ' to -50');
-            self.currentProfit(-50);
-        }, 3000);
     }
 
     return AppViewModel;
