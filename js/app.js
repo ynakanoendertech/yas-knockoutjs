0eefc46a568007618cd1e566459075d80011a0de
Yasuhiro Nakano <ynakano@endertech.com>
2016-12-13 18:24:28 -0800

    Clean up

--- js/app.js
+++ js/app.js
@@ -4,13 +4,7 @@ require(['jquery', 'ko', 'AppViewModel'], function($, ko, AppViewModel) {
 
     $(document).ready(function() {
 
-        ko.components.register('your-component-name', {
-            viewModel: AppViewModel,
-            template: 'some template'
-        });
-
         // Activate
-        //ko.applyBindings(new AppViewModel());
-        ko.applyBindings();
+        ko.applyBindings(new AppViewModel());
     });
 });
