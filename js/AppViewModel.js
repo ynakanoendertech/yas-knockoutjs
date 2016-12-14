9e7c50d9f39fa7f856bb75dc2591ef13269dce69
Yasuhiro Nakano <ynakano@endertech.com>
2016-12-13 17:08:32 -0800

    Clean up

--- js/AppViewModel.js
+++ js/AppViewModel.js
@@ -1,31 +1,9 @@
-define(['jquery', 'jquery-color', 'ko'], function($, jQueryColor, ko) {
+define(['ko'], function(ko) {
     "use strict";
 
     // View model
     function AppViewModel() {
 
-        // Preserve this
-        var self = this;
-
-        self.myItems = ko.observableArray([ 'A', 'B', 'C' ]);
-
-        self.addItem = function() {
-            self.myItems.push("New Item at " + new Date());
-        };
-
-        self.removeItem = function() {
-            self.myItems.remove(this);
-        };
-
-        self.yellowFadeIn = function(element, index, data) {
-            $(element).filter("li")
-                .animate( { backgroundColor: 'yellow' }, 200)
-                .animate( { backgroundColor: 'white' }, 800);
-        };
-
-        self.afterRender = function(element, data) {
-            console.log('afterRender');
-        };
     }
 
     return AppViewModel;
