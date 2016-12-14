f55ba88c53ecea228ae8f93796eb1cb04728a527
Yasuhiro Nakano <ynakano@endertech.com>
2016-12-14 13:31:36 -0800

    Clean up

--- js/AppViewModel.js
+++ js/AppViewModel.js
@@ -3,10 +3,7 @@ define(['ko'], function(ko) {
 
     // View model
     function AppViewModel() {
-        var self = this;
-
-        self.hasCellphone = ko.observable(false);
-        self.cellphoneNumber = "";
+        
     }
 
     return AppViewModel;
