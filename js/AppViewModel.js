ac19c47d886514d3f4c7d83efe8882b605026fce
Yasuhiro Nakano <ynakano@endertech.com>
2016-12-13 17:56:06 -0800

    Use component binding with full syntax

--- js/AppViewModel.js
+++ js/AppViewModel.js
@@ -2,8 +2,12 @@ define(['ko'], function(ko) {
     "use strict";
 
     // View model
-    function AppViewModel() {
+    function AppViewModel(params) {
 
+        // Preserve this
+        var self = this;
+
+        self.text = ko.observable(params && params.initialText || '');
     }
 
     return AppViewModel;
--- js/app.js
+++ js/app.js
@@ -4,7 +4,13 @@ require(['jquery', 'ko', 'AppViewModel'], function($, ko, AppViewModel) {
 
     $(document).ready(function() {
 
+        ko.components.register('message-editor', {
+            viewModel: AppViewModel,
+            template: 'Message: <input data-bind="value: text" /> (length: <span data-bind="text: text().length"></span>)'
+        });
+
         // Activate
-        ko.applyBindings(new AppViewModel());
+        //ko.applyBindings(new AppViewModel());
+        ko.applyBindings();
     });
 });
