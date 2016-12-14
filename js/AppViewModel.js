5f6d752948ca3bb581ed08c07bc25c92efa5f4c6
Yasuhiro Nakano <ynakano@endertech.com>
2016-12-14 13:27:37 -0800

    Clean up

--- js/AppViewModel.js
+++ js/AppViewModel.js
@@ -3,25 +3,7 @@ define(['ko'], function(ko) {
 
     // View model
     function AppViewModel() {
-        var self = this;
 
-        // Private function
-        function showFormId(formElement) {
-            if (formElement.length && formElement.id) {
-                console.dir(formElement.id);
-            }
-        }
-
-        self.doSomething = function(formElement) {
-            showFormId(formElement);
-            console.log('something');
-        };
-
-        self.doSomething2 = function(formElement) {
-            showFormId(formElement);
-            console.log('something 2');
-            return true;
-        };
     }
 
     return AppViewModel;
