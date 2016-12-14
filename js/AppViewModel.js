baa79394468b28fae42187c84d0dc11792803441
Yasuhiro Nakano <ynakano@endertech.com>
2016-12-13 18:37:51 -0800

    Click binding can be used on any visible DOM element

--- js/AppViewModel.js
+++ js/AppViewModel.js
@@ -4,6 +4,15 @@ define(['ko'], function(ko) {
     // View model
     function AppViewModel() {
 
+        // Preserve this
+        var self = this;
+
+        self.numberOfClicks = ko.observable(0);
+
+        self.incrementClickCounter = function() {
+            var previousCount = self.numberOfClicks();
+            self.numberOfClicks(previousCount + 1);
+        };
     }
 
     return AppViewModel;
