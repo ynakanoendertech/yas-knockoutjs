2487f5af2b66cb20ce0e7e8509818d034725b160
Yasuhiro Nakano <ynakano@endertech.com>
2016-12-13 16:08:11 -0800

    Clean up

--- js/AppViewModel.js
+++ js/AppViewModel.js
@@ -4,12 +4,6 @@ define(['ko'], function(ko) {
     // View model
     function AppViewModel() {
 
-        // Preserve this
-        var self = this;
-
-        self.url = ko.observable("year-end.html");
-        self.details = ko.observable("Report including final year-end statistics");
-        self.dataSomething = ko.observable("This is something");
     }
 
     return AppViewModel;
