b7adf805013ad70dde86bacbb9ab8ed0f1eef864
Yasuhiro Nakano <ynakano@endertech.com>
2016-12-14 15:17:38 -0800

    Options binding overwrites model value if no corresponding entry in the list

--- js/AppViewModel.js
+++ js/AppViewModel.js
@@ -5,8 +5,13 @@ define(['ko'], function(ko) {
     function AppViewModel() {
         var self = this;
 
-        self.userName = ko.observable("");
-        self.userPassword = ko.observable("abc");
+        self.countries = ['Japan', 'Bolivia', 'New Zealand'];
+        self.selectedCountry = ko.observable('Latvia');
+
+        // Subscribe function to output value changes
+        self.selectedCountry.subscribe(function(value) {
+            console.log(value);
+        });
     }
 
     return AppViewModel;
