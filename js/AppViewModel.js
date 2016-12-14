276a1f195f2a5f606da62f2b369e48fcda094afc
Yasuhiro Nakano <ynakano@endertech.com>
2016-12-13 18:56:23 -0800

    Clean up

--- js/AppViewModel.js
+++ js/AppViewModel.js
@@ -4,16 +4,6 @@ define(['ko'], function(ko) {
     // View model
     function AppViewModel() {
 
-        // Preserve this
-        var self = this;
-
-        self.myDivHandler = function() {
-            console.log('outer div clicked');
-        };
-
-        self.myButtonHandler = function() {
-            console.log('inner button clicked');
-        };
     }
 
     return AppViewModel;
