42dc94321889ead866bc901a5292a8bd5e0afc67
Yasuhiro Nakano <ynakano@endertech.com>
2016-12-14 17:23:00 -0800

    Use name and data parameters for template binding

--- js/AppViewModel.js
+++ js/AppViewModel.js
@@ -3,7 +3,10 @@ define(['ko'], function(ko) {
 
     // View model
     function AppViewModel() {
+        var self = this;
 
+        self.buyer = { name: 'Franklin', credits: 250 };
+        self.seller = { name: 'Mario', credits: 5800 };
     }
 
     return AppViewModel;
