c73f258d2c3835616253c955a8580e4cabd1e5e0
Yasuhiro Nakano <ynakano@endertech.com>
2016-12-14 16:46:52 -0800

    Clean up

--- js/AppViewModel.js
+++ js/AppViewModel.js
@@ -3,15 +3,7 @@ define(['ko'], function(ko) {
 
     // View model
     function AppViewModel() {
-        var self = this;
 
-        self.availableCountries = ko.observableArray(['France', 'Germany', 'Spain']);
-        self.chosenCountries = ko.observableArray(['Germany']);
-
-        setTimeout(function() {
-            console.log('choose one more item');
-            self.chosenCountries.push('France');
-        }, 3000);
     }
 
     return AppViewModel;
