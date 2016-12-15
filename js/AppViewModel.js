c12e807d71d41968dbf061ff7e5f24d30c8305e1
Yasuhiro Nakano <ynakano@endertech.com>
2016-12-14 16:34:56 -0800

    Clean up

--- js/AppViewModel.js
+++ js/AppViewModel.js
@@ -3,19 +3,7 @@ define(['ko'], function(ko) {
 
     // View model
     function AppViewModel() {
-        var self = this;
 
-        self.myItems = [
-            { name: 'Item 1', id: 1, disable: ko.observable(false)},
-            { name: 'Item 3', id: 3, disable: ko.observable(true)},
-            { name: 'Item 4', id: 4, disable: ko.observable(false)}
-        ];
-
-        self.setOptionDisable = function(option, item) {
-            console.dir(option);
-            console.dir(item);
-            ko.applyBindingsToNode(option, { disable: item.disable }, item);
-        };
     }
 
     return AppViewModel;
