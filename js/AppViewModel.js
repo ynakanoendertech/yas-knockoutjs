d2d3c97e915519934b341177334e005a5d01596b
Yasuhiro Nakano <ynakano@endertech.com>
2016-12-14 16:12:00 -0800

    Clean up

--- js/AppViewModel.js
+++ js/AppViewModel.js
@@ -3,15 +3,7 @@ define(['ko'], function(ko) {
 
     // View model
     function AppViewModel() {
-        var self = this;
 
-        self.wantsSpam = ko.observable(true);
-        self.spamFlavors = ko.observable("almond");
-
-        setTimeout(function() {
-            console.log('change values');
-            self.spamFlavors('msg');
-        }, 3000);
     }
 
     return AppViewModel;
