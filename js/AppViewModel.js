76b2a42f8e3039898ede80584d6f69ce4a618958
Yasuhiro Nakano <ynakano@endertech.com>
2017-01-03 14:23:21 -0800

    Clean up

--- js/AppViewModel.js
+++ js/AppViewModel.js
@@ -5,27 +5,6 @@ define(['ko'], function(ko) {
     function AppViewModel() {
         var self = this;
 
-        self.employees = ko.observableArray([
-            { name: "Kari", active: ko.observable(true) },
-            { name: "Bryan", active: ko.observable(false) },
-            { name: "Nora", active: ko.observable(false) }
-        ]);
-
-        self.displayMode = function(employee, bindingContext) {
-
-            console.dir(bindingContext["$parent"]);
-
-            var myName = bindingContext["$rawData"].name;
-            if (myName == "Nora") {
-                employee.active(true);
-            }
-
-            return employee.active() ? "active" : "inactive";
-        };
-
-        setTimeout(function() {
-            self.employees()[1].active(true);  // Set active template for "Bryan"
-        }, 3000);
     }
 
     return AppViewModel;
