4530763f5e00b71913cec2135d957fd95c40254d
Yasuhiro Nakano <ynakano@endertech.com>
2017-01-03 15:24:11 -0800

    Use whitespaces in bindings

--- js/AppViewModel.js
+++ js/AppViewModel.js
@@ -5,20 +5,12 @@ define(['ko'], function(ko) {
     function AppViewModel() {
         var self = this;
 
-        self.shouldShowMessage = ko.observable(true);
-        self.price = ko.observable(51);
-        self.cellphoneNumber = ko.observable('111-222-3333');
-        self.parseAreaCode = function(cellNum) {
-            return cellNum.substring(0, 3);
-        };
-        self.myFunctionValue = ko.observable('initial value');
-        self.myFunction = function(param, data) {
-            self.myFunctionValue(param);
-        };
-
-        setTimeout(function() {
-            self.cellphoneNumber('555-666-7777');
-        }, 3000);
+        self.selectedCountry = ko.observable();
+        self.availableCountries = ko.observableArray([
+            { countryName: 'France' },
+            { countryName: 'Germany' },
+            { countryName: 'Spain' }
+        ]);
     }
 
     return AppViewModel;
