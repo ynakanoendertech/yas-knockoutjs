628f5d0396f15234ab337fefe81ba12d08a873d7
Yasuhiro Nakano <ynakano@endertech.com>
2017-01-03 15:18:53 -0800

    Use $data to bind property name with hypen

--- js/AppViewModel.js
+++ js/AppViewModel.js
@@ -5,8 +5,20 @@ define(['ko'], function(ko) {
     function AppViewModel() {
         var self = this;
 
-        self.hasCellphone = ko.observable(true);
-        self.cellphoneNumber = ko.observable("111-222-3333");
+        self.shouldShowMessage = ko.observable(true);
+        self.price = ko.observable(51);
+        self.cellphoneNumber = ko.observable('111-222-3333');
+        self.parseAreaCode = function(cellNum) {
+            return cellNum.substring(0, 3);
+        };
+        self.myFunctionValue = ko.observable('initial value');
+        self.myFunction = function(param, data) {
+            self.myFunctionValue(param);
+        };
+
+        setTimeout(function() {
+            self.cellphoneNumber('555-666-7777');
+        }, 3000);
     }
 
     return AppViewModel;
