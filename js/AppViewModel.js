ceff4b46d16406fb2cd040b0f79bb7cc73e36d47
Yasuhiro Nakano <ynakano@endertech.com>
2017-01-04 19:32:56 -0800

    Use extend function to pass values to descendant bindings

--- js/AppViewModel.js
+++ js/AppViewModel.js
@@ -2,12 +2,26 @@ define(['ko'], function(ko) {
     "use strict";
 
     // Custom bindings
+
     ko.bindingHandlers.allowBindings = {
         init: function(element, valueAccessor) {
             // Let bindings proceed as normal *only if* my value is false
             var shouldAllowBindings = ko.unwrap(valueAccessor());
             return {
                 controlsDescendantBindings: !shouldAllowBindings
+            };
+        }
+    };
+
+    ko.bindingHandlers.withProperties = {
+        init: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
+            // Make a modified binding context, with an extra properties, and apply it to descendant elements
+            var innerBindingContext = bindingContext.extend(valueAccessor);
+            ko.applyBindingsToDescendants(innerBindingContext, element);
+
+            // Also tell KO *not* to bind the descendants itself, otherwise they will be bound twice
+            return {
+                controlsDescendantBindings: true
             };
         }
     };
