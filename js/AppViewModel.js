fec9b58dd1eb7eb860d067a8d3097cafd24190b4
Yasuhiro Nakano <ynakano@endertech.com>
2017-01-04 19:08:49 -0800

    Register event handlers at init callback to change observable

--- js/AppViewModel.js
+++ js/AppViewModel.js
@@ -44,12 +44,38 @@ define(['ko'], function(ko) {
         }
     };
 
+    ko.bindingHandlers.hasFocus = {
+        init: function(element, valueAccessor) {
+
+            // Register focus event handler at init callback
+            $(element).focus(function() {
+                var value = valueAccessor();
+                value(true);
+            });
+
+            // Register blur event handler at init callback
+            $(element).blur(function() {
+                var value = valueAccessor();
+                value(false);
+            });
+        },
+        update: function(element, valueAccessor) {
+            var value = valueAccessor();
+            if (ko.unwrap(value)) {
+                element.focus();
+            } else {
+                element.blur();
+            }
+        }
+    };
+
     // View model
     function AppViewModel() {
         var self = this;
 
         self.giftWrap = ko.observable(false);
         self.giftCard = ko.observable(false);
+        self.editingName = ko.observable(false);
     }
 
     return AppViewModel;
