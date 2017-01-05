ceea9202053f5828e200d4d213346f768aa2c4a0
Yasuhiro Nakano <ynakano@endertech.com>
2017-01-04 19:23:27 -0800

    Use controlsDescendantBindings to allow affecting all descendant

--- js/AppViewModel.js
+++ js/AppViewModel.js
@@ -1,6 +1,17 @@
 define(['ko'], function(ko) {
     "use strict";
 
+    // Custom bindings
+    ko.bindingHandlers.allowBindings = {
+        init: function(element, valueAccessor) {
+            // Let bindings proceed as normal *only if* my value is false
+            var shouldAllowBindings = ko.unwrap(valueAccessor());
+            return {
+                controlsDescendantBindings: !shouldAllowBindings
+            };
+        }
+    };
+
     // View model
     function AppViewModel() {
         var self = this;
